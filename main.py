5400a78a6638312d0ed65390a3c47253146de0a3
textstorm <jiangzhewise@gmail.com>
2017-11-18 16:00:01 +0800

    main update

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 
+import numpy as np
+import gym
+import ddpg
+
+def main(args):
+  env = gym.make(args.env_name)
+  agent = ddpg.DDPG(args, env, name='ddpg')
+  for episode in range(args.nb_episodes):
+    state = env.reset()
+    for step in range(args.nbsteps):
+      action = agent.actor()
+
+if __name__ == '__main__':
+  args = config.get_args()
+  main(args)
